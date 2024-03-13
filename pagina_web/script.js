2d0f42fe682c12e2e1405c1dcbcde356ace9eb6f
aLeDv7 <163217027+aLeDv7@users.noreply.github.com>
2024-03-12 19:50:11 -0500

    Initial commit

---
+++ pagina_web/script.js
@@ -0,0 +1,52 @@
+// alert("Hola este es mi Javascript");
+
+//////////////////////////////////////////////////
+
+// let nombre = "Alejandro";
+
+// var nombre1 = "Alejandro";
+
+// const nombre2 = "Alejandro";
+
+
+// console.log (nombre);
+// console.log (nombre1);
+// console.log (nombre2);
+
+
+//////////////////////////////////////////////////
+
+//**SELECCIONAR ELEMENTOS**
+
+// let ContenidoTitulo = "Acerca de mi"
+
+// let titulo = document.querySelector("#section-principal .section-1 .acerca-de h2")
+// titulo.innerHTML = ContenidoTitulo;
+
+
+//**CONDICIONALES**
+
+// let textoTitulo = titulo.innerText;
+// console.log(textoTitulo);
+
+// if(textoTitulo == "Acerca de mi"){
+//     titulo.innerHTML = "Otro";
+// } else {
+//     console.log("no se cumple");
+// }
+
+//////////////////////////////////////////////////
+
+let nombre = "Julio";
+let ciudad = "San Jerónimo";
+let gusto = "tenis";
+
+let parrafo = document.querySelector(".parrafo1");
+
+function cambiarTexto(nombre, ciudad, gusto) {
+    let contenido = `Hola, mi nombre es ${nombre}, nací en ${ciudad} y vivo en Lima. Me gusta el ${gusto}, correr y viajar. Me encantaría aprender a programar para poder ayudar las personas a mostrar lo que hacen`;
+    
+    return contenido;
+    }
+
+parrafo.innerText  = cambiarTexto(nombre, ciudad, gusto);
